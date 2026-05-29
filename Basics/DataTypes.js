420c2fef9e2f919cf289554c92eac9de1c2652b9
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-05-29 15:54:42 +0530

    DataTypes.js commited

---
+++ Basics/DataTypes.js
@@ -0,0 +1,14 @@
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint 
+// string 
+// boolean
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+console.log(typeof "hitesh")
