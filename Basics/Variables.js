8343df406c30b240a35813c0d0374f4b4fe7ceca
Yash Sharma <yashsharmaji8502@gmail.com>
2026-05-29 14:12:28 +0530

    Variables.js file added

---
+++ Basics/Variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+
+console.log(3+3) ;
+console.log(3-3) ;
+
+// var is not used in the standard practice of java
+// This is because of the problem such as function scoped not block scoped
+// Another problem is of hoisting and such things
