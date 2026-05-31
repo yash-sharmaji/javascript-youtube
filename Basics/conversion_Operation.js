54b2f3305863e144b1bed106cdd545a5d62c4ac6
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-05-31 21:21:02 +0530

    Added Conversion-Operation file

---
+++ Basics/conversion_Operation.js
@@ -0,0 +1,20 @@
+console.log(2/3)
+console.log(2%3)
+console.log(2.0**3)
+console.log(0.1 + 0.2)
+
+let str1="Hello"
+let str2=" Yash"
+
+let str3 = str1+str2
+console.log(str3);
+console.log("1" + 2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+console.log(3 + 4 * 5 % 3) //5
+// Use parenthesis in the practical java codes 
+
+console.log(+true); // 1
+console.log(+"") //0
+
