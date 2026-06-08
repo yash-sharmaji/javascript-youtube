2a1e7e4ba0d2543fced34c7ab4e90275b99d44bb
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-06-08 16:01:58 +0530

    Chatper 2 pushed

---
+++ BC_01/script.js
@@ -0,0 +1,28 @@
+console.log(`Hello`);
+
+//This gets outputed on the console part of the inspect
+
+//window.alert(`This is an alert!`);
+// window.alert(`This is an alert`);
+
+document.getElementById("header1").textContent = 'Hello';
+document.getElementById("para1").textContent = `I like Pizza`;
+
+// The 2nd part now begins --->
+
+//variable = a container that stores a value and behave that if it
+//           were the value that it contains 
+
+// 1. declaration    let x;
+// 2. assignment     x=100;
+
+let x=100;
+let price = 10.99;
+let gpa = 2.1;
+
+let trial = "Yash";
+console.log(x);
+console.log(`You are ${x} years old`);
+console.log(`you are having ${gpa} gpa`);
+
+console.log(typeof (trial));
