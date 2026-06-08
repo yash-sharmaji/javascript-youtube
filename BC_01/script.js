6712a8ffd0083d031b73874e32850882053de77e
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-06-08 16:26:50 +0530

    Added the 3rd lesson

--- BC_01/script.js
+++ BC_01/script.js
@@ -8,7 +8,12 @@ console.log(`Hello`);
 document.getElementById("header1").textContent = 'Hello';
 document.getElementById("para1").textContent = `I like Pizza`;
 
+
+
 // The 2nd part now begins --->
+
+
+
 
 //variable = a container that stores a value and behave that if it
 //           were the value that it contains 
@@ -26,3 +31,35 @@ console.log(`You are ${x} years old`);
 console.log(`you are having ${gpa} gpa`);
 
 console.log(typeof (trial));
+document.getElementById("para1").textContent = x;
+
+//hence the text alterations can be provided with the help of the variables 
+
+
+
+// The 3rd part now begins
+
+
+
+// arithmetic operators = operands(values, variables,etc..)
+//                        operators (+,-,*,/);
+
+
+let students =30;
+students=students+1;
+students=students-2;
+
+students = students/2;
+students =students ** 2;
+
+console.log(students);
+
+students +=1; //augumented assignment operators -= /= %= **=
+
+console.log(students);
+
+// operator precedence ->
+// parenthesis -> exponents -> multiplication & divison & modulo -> additon & subtraction 
+
+let result = 1+2*3+4**2;
+console.log(result);
