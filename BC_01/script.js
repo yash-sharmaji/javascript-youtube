6c7cff98a911b84e9b96f49270a43f26c818392c
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-06-09 13:48:49 +0530

    Added the 7th part

--- BC_01/script.js
+++ BC_01/script.js
@@ -116,3 +116,35 @@ document.getElementById("calculate").onclick = machine = () => {
     let answer = 3.14 * getradius * getradius;
     console.log(answer);
 }
+
+
+
+// The 7th part of the video begins here -->
+
+
+
+
+//Counter Program 
+
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn =document.getElementById("increaseBtn");
+const countlabel = document.getElementById("countlabel");
+
+let count = 0 ;
+
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count=0;
+    countlabel.textContent = count;
+}
