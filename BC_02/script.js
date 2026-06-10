7392a8cc4fac7be36adb24758c95be240b214642
yash-sharmaji <yashsharmaji8502@gmail.com>
2026-06-10 11:04:43 +0530

    Added the 9th part

--- BC_02/script.js
+++ BC_02/script.js
@@ -4,4 +4,39 @@
 //       of properties and methods
 
 
+/* Math.[round , floor , ceil , trunc , pow(x,y) , sqrt(x) , log(x) , sin(x) , cos(x) , tan(x) , abs(x) , sign(x)] */
 
+/* Math.max(x,y,z) ; Math.min(x,y,z) */
+
+
+// Random Number Generator 
+
+// let randomNum = Math.ceil(Math.random()*6);
+// console.log(randomNum);
+
+// To output a number between the a minimum and a maximum 
+
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max-min)) + min;
+
+// another function is using modulo - 
+// let randomNum = (Math.floor(Math.random() * (max) + min))%max;
+
+// console.log(randomNum);
+
+// Making a actual dice 
+
+const MyButton = document.getElementById("Mybutton");
+const MyLabel = document.getElementById("Mylabel");
+
+const min = 1;
+const max = 6;
+
+
+MyButton.addEventListener("click",()=>{
+    // let randomNum = Math.ceil(Math.random()*6);
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    MyLabel.textContent=randomNum;
+});
